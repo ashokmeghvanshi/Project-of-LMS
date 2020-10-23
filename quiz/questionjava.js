adf61f9ad80bb78e2b540a078c4bc46e1a360145
ASHOK KUMAR MEGHVANSHI <42780456+ashokmeghvanshi@users.noreply.github.com>
2020-10-23 12:36:08 +0530

    Images and Quiz of LMS

---
+++ quiz/questionjava.js
@@ -0,0 +1,81 @@
+var questions = [{
+	"question": "C is ?",
+	"option1": " A third generation high level language",
+	"option2": "A machine language",
+	"option3": "An assembly language",
+	"option4": "All of the above",
+	"answer": "1"
+},
+{
+	"question": "If a variable is a pointer to a structure, then which of the following operator is used to access data members of the structure through the pointer variable?",
+	"option1": "&",
+	"option2": "#",
+	"option3": "*",
+	"option4": "->",
+	"answer": "4"
+},
+{
+	"question": "What is the similarity between a structure, union and enumeration?",
+	"option1": "All of them let you define new data types",
+	"option2": "All of them let you define new values",
+	"option3": "All of them let you define new pointers",
+	"option4": "All of them let you define new array",
+	"answer": "1"
+},
+{
+	"question": "How will you free the allocated memory ?",
+	"option1": "remove(var-name);",
+	"option2": "delete(var-name);",
+	"option3": "free(var-name);",
+	"option4": "dalloc(var-name);",
+	"answer": "3"
+},
+{
+	"question": "Which bitwise operator is suitable for turning on a particular bit in a number?",
+	"option1": "& operator",
+	"option2": "&& operator",
+	"option3": "| operator",
+	"option4": "|| operator",
+	"answer": "3"
+},
+{
+	"question": "What are the types of linkages?",
+	"option1": " Internal",
+	"option2": "External, Internal and None",
+	"option3": "External and None",
+	"option4": "External, Internal ",
+	"answer": "2"
+},
+{
+	"question": "Which of the following concepts means determining at runtime what method to invoke?",
+	"option1": "Data hiding ",
+	"option2": " Dynamic Typing",
+	"option3": "Dynamic Loading",
+	"option4": "Dynamic binding",
+	"answer": "4"
+},
+{
+	"question": "Which of the following type of class allows only one object of it to be created?",
+	"option1": "Singleton class",
+	"option2": " Abstract class",
+	"option3": " Friend class",
+	"option4": "virtual class",
+	"answer": "1"
+},
+{
+	"question": "Which of the following statements is correct?",
+	"option1": "Pointer to base class cannot be created.",
+	"option2": "Base class pointer cannot point to Derived class.",
+	"option3": "Pointer to Derived class cannot be created.",
+	"option4": "Derived class pointer cannot point to base class.",
+	"answer": "4"
+},
+{
+	"question": "Which of the following statement is correct?",
+	"option1": " A constructor is called at the time of declaration of a class.",
+	"option2": " A constructor is called at the time of use of an object.",
+	"option3": " A constructor is called at the time of declaration of a object.",
+	"option4": " A constructor is called at the time of use of a class.",
+	"answer": "3"
+},
+]
---
+++ quiz/questionpython.js
@@ -0,0 +1,81 @@
+var questions = [{
+	"question": "What is the maximum possible length of an identifier?",
+	"option1": "31 characters",
+	"option2": "63 characters",
+	"option3": "79 characters",
+	"option4": "none of the mentioned",
+	"answer": "4"
+},
+{
+	"question": "Which of the following is not a keyword?",
+	"option1": "eval",
+	"option2": "assert",
+	"option3": "nonlocal",
+	"option4": "pass",
+	"answer": "1"
+},
+{
+	"question": ">>>max(what are you)",
+	"option1": "error",
+	"option2": "u",
+	"option3": "t",
+	"option4": "y",
+	"answer": "4"
+},
+{
+	"question": "Suppose d = {“john”:40, “peter”:45}. To obtain the number of entries in dictionary which command do we use?",
+	"option1": "d.size()",
+	"option2": "len(d)",
+	"option3": "size(d)",
+	"option4": "d.len()",
+	"answer": "2"
+},
+{
+	"question": "The one’s complement of 110010101 is:",
+	"option1": "001101010",
+	"option2": "110010101",
+	"option3": "001101011",
+	"option4": "110010100",
+	"answer": "1"
+},
+{
+	"question": "Suppose list1 = [0.5 * x for x in range(0, 4)], list1 is:",
+	"option1": "[0, 1, 2, 3]",
+	"option2": "[0, 1, 2, 3, 4]",
+	"option3": "[0.0, 0.5, 1.0, 1.5]",
+	"option4": "[0.0, 0.5, 1.0, 1.5, 2.0]",
+	"answer": "3"
+},
+{
+	"question": "To add a new element to a list we use which command?",
+	"option1": "list1.add(5)",
+	"option2": "list1.append(5)",
+	"option3": "list1.addLast(5)",
+	"option4": "list1.addEnd(5)",
+	"answer": "2"
+},
+{
+	"question": "Suppose list1 is [3, 4, 5, 20, 5], what is list1.index(5)?",
+	"option1": "0",
+	"option2": "1",
+	"option3": "4",
+	"option4": "2",
+	"answer": "4"
+},
+{
+	"question": "Suppose list1 is [3, 4, 5, 20, 5, 25, 1, 3], what is list1.count(5)?",
+	"option1": "3",
+	"option2": "4",
+	"option3": "2",
+	"option4": "7",
+	"answer": "3"
+},
+{
+	"question": ". The assignment of more than one function to a particular operator is _______",
+	"option1": "Operator over-assignment",
+	"option2": "Operator overriding",
+	"option3": "Operator overloading",
+	"option4": "Operator instance",
+	"answer": "3"
+},
+]
